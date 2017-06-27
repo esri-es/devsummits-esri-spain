6ec82a6f1bf12a3079060934d6c8b0e294e6b53b
RaulJimenezOrtega <hhkaos@gmail.com>
2017-06-27 21:50:46 +0200

    Removing personal slides

--- 2017/ppts/sesion-inaugural-raul/demos/3d-external-renderer-wind-global/typings/arcgis-js-api-extensions.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "esri/layers/BaseTileLayer" {
-    const BaseTileLayer: any;
-    export = BaseTileLayer;
-}
--- 2017/ppts/sesion-inaugural-raul/demos/3d-external-renderer-wind-global/typings/gl-matrix.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'https://cdnjs.cloudflare.com/ajax/libs/gl-matrix/2.3.2/gl-matrix-min.js' {
-  import * as glMatrix from 'gl-matrix';
-  export = glMatrix;
-}
